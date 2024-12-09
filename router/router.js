0bbd636693527cbb5ff36bf1516f630dab90eba9
Sendiko <sarangtawon897@gmail.com>
2024-12-09 22:05:26 +0700

    enc(add): few new routes

--- router/router.js
+++ router/router.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const adminController = require("../controllers/admin_controller");
 const categoryController = require("../controllers/category_controller");
 const favoriteController = require("../controllers/favorite_controller");
+const ingredientController = require("../controllers/ingredient_controller");
+const memberController = require("../controllers/member_controller");
+const nutritionistController = require("../controllers/nutritionist_controller");
 
 router.get("/admin", adminController.data);
 router.get("/admin/:adminID", adminController.index);
@@ -22,5 +25,23 @@ router.get("/favorite/id/:favoriteID", favoriteController.index);
 router.post("/favorite", favoriteController.store);
 router.put("/favorite/:favoriteID", favoriteController.update);
 router.delete("/favorite/:favoriteyID", favoriteController.delete);
+
+router.get("/ingredient/", ingredientController.data);
+router.get("/ingredient/id/:ingredientID", ingredientController.index);
+router.post("/ingredient", ingredientController.store);
+router.put("/ingredient/:ingredientID", ingredientController.update);
+router.delete("/ingredient/:ingredientID", ingredientController.delete);
+
+router.get("/member/", ingredientController.data);
+router.get("/member/id/:memberID", ingredientController.index);
+router.post("/member", ingredientController.store);
+router.put("/member/:memberID", ingredientController.update);
+router.delete("/member/:memberID", ingredientController.delete);
+
+router.get("/member/", ingredientController.data);
+router.get("/member/id/:memberID", ingredientController.index);
+router.post("/member", ingredientController.store);
+router.put("/member/:memberID", ingredientController.update);
+router.delete("/member/:memberID", ingredientController.delete);
 
 module.exports = router;
